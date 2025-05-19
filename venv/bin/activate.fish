f1486c22bb1b4c6072f4ffdce1e35e5f946d0ab7
panther <panther@rpi5.local>
2025-05-19 15:25:31 +0530

    new

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -1,5 +1,5 @@
 # This file must be used with "source <venv>/bin/activate.fish" *from fish*
-# (https://fishshell.com/). You cannot run it directly.
+# (https://fishshell.com/); you cannot run it directly.
 
 function deactivate  -d "Exit virtual environment and return to normal shell environment"
     # reset old environment variables
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/abhinnvyas/Desktop/Data Drive/3PX Vision Private Limited/RTSP Feed/venv'
+set -gx VIRTUAL_ENV /home/panther/Projects/RTSP-Feed/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
